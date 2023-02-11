ec65f84447f71270b10691fd61284740d3d9c4a3
lexuanbinh <binh.le.xuan@gmail.com>
2023-02-11 14:45:39 +0700

    update new quota files

---
+++ scripts/PrepareData.js
@@ -0,0 +1,25 @@
+var quota_data;
+var download_time;
+var total_quota = 2500;
+var completed_interview;
+/************************************/
+function prepareData() {
+  raw_data = JSON.parse(weekly_plan);
+ 
+  var i = 0;
+  completed_interview = 0;
+  quota_data = [];
+  quota_data.length = 0;
+  for (i = 0; i<raw_data.length; i++ )
+  {
+    var item = raw_data[i];
+    if (item.Completed) 
+    {
+      completed_interview = completed_interview + item.Completed;
+    }
+    if (item && item.Missing>0) 
+    {
+      quota_data.push(item);
+    }
+  }
+}
